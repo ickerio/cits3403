e44e8f8f486d99d1884d141ad2ca23154f7b7ec7
ickerio <harry.ickeringill@gmail.com>
2024-04-09 16:41:26 +0800

    add temp user and navbar dropdown

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,22 +4,30 @@ from flask import (
 
 app = Flask(__name__)
 
+def get_user():
+    return {
+        'username': 'johndoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+    }
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', **get_user())
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', **get_user())
 
 @app.route('/find-sketch')
 def findSketch():
-    return render_template('find-sketch.html')
+    return render_template('find-sketch.html', **get_user())
 
 @app.route('/guess-sketch')
 def guessSketch():
-    return render_template('guess-sketch.html')
+    return render_template('guess-sketch.html', **get_user())
 
 @app.route('/draw-sketch')
 def drawSketch():
-    return render_template('draw-sketch.html')
+    return render_template('draw-sketch.html', **get_user())
