59b17aadf746b772e403012976a3140ebbaa4ffa
ickerio <harry.ickeringill@gmail.com>
2024-04-09 17:35:16 +0800

    Home (index) page to find sketches

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,10 +11,36 @@ def get_user():
         'last_name': 'Doe',
     }
 
-
 @app.route('/')
 def index():
-    return render_template('index.html', **get_user())
+    sketches = [
+        {
+            'id': 1,
+            'username': 'someranomduser',
+            'date': '4m ago', # obviously this is a placeholder for better date parsing
+            'has_guessed': False,
+        },
+        {
+            'id': 2,
+            'username': 'otheruser',
+            'date': '7h ago', # obviously this is a placeholder for better date parsing
+            'has_guessed': False,
+        },
+        {
+            'id': 3,
+            'username': 'anotheruser',
+            'date': 'Apr 4', # obviously this is a placeholder for better date parsing
+            'has_guessed': True,
+        },
+        {
+            'id': 4,
+            'username': 'yetanotheruser',
+            'date': 'Dec 12, 2023', # obviously this is a placeholder for better date parsing
+            'has_guessed': True,
+        }
+    ]
+
+    return render_template('index.html', **get_user(), sketches=sketches)
 
 @app.route('/login')
 def login():
