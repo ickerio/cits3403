b5d44b52398082d0dc4fb5f50f3814d6bc74b000
ickerio <harry.ickeringill@gmail.com>
2024-04-09 15:46:31 +0800

    create home page and base layout

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,10 @@ from flask import (
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
 def login():
     return render_template('login.html')
 
