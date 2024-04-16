9fc001dd164a979855511c2a6fdb70968c7f8368
ansu <161705621+ansuuuuuuu@users.noreply.github.com>
2024-04-17 00:08:15 +0800

    updated/cleaned

--- flaskr/static/js/guess.js
+++ flaskr/static/js/guess.js
@@ -1,66 +1,19 @@
 const countdownTimer = document.getElementById('countdowntimer');
+const countdownText = document.getElementById('countdownMessage');
+const timeRemainingText = document.getElementById('countdowntext');
 let timeLeft = 10;
 let timerRunning = true;
-let attempts = 0;
-let correct = false;
-let incorrectAlertShown = false;
-let oneMoreTryAlertShown = false;
 
 // Start the timer
 const downloadTimer = setInterval(() => {
-    countdownTimer.textContent = timeLeft;
-    timeLeft--;
-
-    if (timeLeft < 0) {
+    if (timeLeft <= 0) {
         clearInterval(downloadTimer);
         timerRunning = false;
-        if (!correct && !incorrectAlertShown) {
-            incorrectAlertShown = true;
-            setTimeout(() => {
-                alert("Time's up :( ");
-                document.getElementById('userguess').setAttribute('disabled', true); // disable input
-                document.getElementById('submitguessBtn').setAttribute('disabled', true); // disable button
-            }, 1000);
-        }
-    }
-}, 1000);
-
-document.getElementById("submitguessBtn").addEventListener("click", function() {
-    if (!timerRunning) return; // If time is up, do not evaluate
-
-    // add the number of attempts
-    attempts++;
-
-    // check
-    checkGuess();
-});
-
-//check the guess
-function checkGuess() {
-    var userGuess = document.getElementById('userguess').value;
-    var wordPlaceholder = document.getElementById('wordPlaceholder').textContent // word/prompt that sketcher used to draw
-    userGuess = userGuess.toLowerCase();
-    wordPlaceholder = wordPlaceholder.toLowerCase();
-    if (userGuess === wordPlaceholder) {
-        // when correct
-        if (!correct) {
-            correct = true;
-            alert("Correct");
-            document.getElementById('userguess').setAttribute('disabled', true); // disable input
-            document.getElementById('submitguessBtn').setAttribute('disabled', true); // disable button
-            clearInterval(downloadTimer); // Stop the timer
-        }
+        document.getElementById('submitguessBtn').setAttribute('disabled', true); // Disable button
+        countdownText.classList.remove('hidden'); 
+        timeRemainingText.classList.add('hidden'); 
     } else {
-        //  when incorrect
-        if (attempts === 1 && !oneMoreTryAlertShown) {
-            oneMoreTryAlertShown = true;
-            alert("Incorrect. You have one more try.");
-        } else if (attempts >= 2 && !incorrectAlertShown) {
-            incorrectAlertShown = true;
-            alert("Incorrect");
-            document.getElementById('userguess').setAttribute('disabled', true); // disable input
-            document.getElementById('submitguessBtn').setAttribute('disabled', true); // disable button
-            clearInterval(downloadTimer); // stop timer
-        }
+        countdownTimer.textContent = timeLeft;
     }
-}
+    timeLeft--; // Decrease timeLeft after updating countdownTimer.textContent
+}, 1000);
