18fc77e727d23560132a376f745d76b7fe5849f8
JJ <75469742+jjwilliamjun@users.noreply.github.com>
2024-05-19 13:10:37 +0800

    Update forms.py with email editing

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, InputRequired, Length, Regexp, EqualTo, Email
+from wtforms.validators import DataRequired, InputRequired, Length, Regexp, EqualTo, Email, Optional
 
 class ExampleForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -37,21 +37,31 @@ class login_form(FlaskForm):
     submit = SubmitField('Log in')
 
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, InputRequired, Length, Regexp, EqualTo, Email
+
 class ProfileForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=30)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=30)])
     username = StringField(
+        'Username',
         validators=[
             InputRequired(),
-            Length(3, 20, message="Please provide a valid name"),
+            Length(min=3, max=20, message="Please provide a valid name"),
             Regexp(
                 "^[A-Za-z][A-Za-z0-9_.]*$",
                 0,
                 "Usernames must have only letters, numbers, dots, or underscores",
             ),
         ]
-    )  
+    )
+    email = StringField('Email', validators=[DataRequired(), Email(message="Please enter a valid email address.")])
     password = PasswordField('Current Password', validators=[DataRequired()])
-    new_password = PasswordField('New Password', validators=[InputRequired(), Length(8, 72)])
-    confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password', message='Passwords must match')])
+    new_password = PasswordField('New Password', validators=[Optional(), Length(min=8, max=72)])
+    confirm_password = PasswordField(
+        'Confirm New Password',
+        validators=[EqualTo('new_password', message='Passwords must match')]
+    )
     submit = SubmitField('Save Changes')
+
